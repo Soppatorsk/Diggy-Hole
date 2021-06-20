c1bf2ac6f0ebb80f9c79d7fd4311debcba99e650
Simon Karlsson <simon.kevlarsjal@gmail.com>
2021-06-20 20:26:50 +0200

    test

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 
 public class Main : MonoBehaviour
 {
+    //test
     public GameObject goldDisplay;
     public GameObject autoIncDisplay;
     public Shop newShop = new Shop();
